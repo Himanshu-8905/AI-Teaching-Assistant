32a9108d5741eb29033e4feea79c9eee380fafa7
Himanshu-8905 <kumawatbasant555@gmail.com>
2026-03-16 00:01:08 +0530

    project init , connect db and add User schema

---
+++ server/auth/model.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class StudentUser(BaseModel):
+    fullname:str
+    email:str
+    username:str
+    password:str
+    grade:int
+    school:str
+
+
+class TeacherUser(BaseModel):
+    fullname:str
+    email:str
+    username:str
+    password:str
+    school:str
---
+++ server/config/db.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME","test_db")
