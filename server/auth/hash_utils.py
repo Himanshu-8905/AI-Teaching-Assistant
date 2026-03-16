7b97ad4842704b8a13bb13d399856b119fe3b813
Himanshu-8905 <kumawatbasant555@gmail.com>
2026-03-16 15:39:50 +0530

    add Auth and Auth routes

---
+++ server/auth/hash_utils.py
@@ -0,0 +1,11 @@
+import bcrypt
+
+def hash_password(plain_password:str)->str:
+    """Hash a Plain Password using bcrypt."""
+    hashed = bcrypt.hashpw(plain_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed.decode('utf-8')
+
+def verify_password(plain_password:str , hashed_password:str)-> bool:
+    """verify a Plain password against a hashed password."""
+    return bcrypt.checkpw(plain_password.encode('utf-8'),hashed_password.encode('utf-8'))
+
--- server/config/db.py
+++ server/config/db.py
@@ -6,3 +6,9 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME","test_db")
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+# User collection
+users_collection = db["users"]
